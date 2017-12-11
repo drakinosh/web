ab99576e1906c1dd2b43df173d1f5a79565a028e
drakinosh <sanskarchand@gmail.com>
2017-12-11 08:45:14 +0545

    sql dump updated

--- forum_proj/proj_db_backup.sql
+++ forum_proj/proj_db_backup.sql
@@ -30,7 +30,7 @@ CREATE TABLE `posts` (
   `image` blob,
   `details` text COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`pid`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (1,1,2,'2017-12-06 17:55:44',NULL,'Miraak? Is that you?'),(1,2,1,'2017-12-11 00:28:14',NULL,'Me: [b] FUS ROH DAH! [/b]\r\n[i]Testing BBcode parser [/i]'),(1,3,1,'2017-12-11 00:29:27',NULL,'<em>Trying again this time</em>.\r\n<strong> Fus Ro Dah! </strong>'),(1,4,1,'2017-12-11 00:34:04',NULL,'[url]htpp://www.fanfiction.net[/url]\r\n\r\n<span style=\"text-decoration:underline;\">What the?</span>.'),(1,5,1,'2017-12-11 00:34:22',NULL,'[link]http://www.example.com[/link]'),(1,6,1,'2017-12-11 00:35:23',NULL,'<em>Italics</em>'),(1,7,1,'2017-12-11 00:38:36',NULL,'[url href=\"http://www.fanfiction.net/alert/story.php\"] click me [/url]'),(1,8,1,'2017-12-11 00:39:46',NULL,'[url=\"http://www.fanfiction.net/alert/story.php\"] click me [/url]'),(1,9,1,'2017-12-11 00:40:08',NULL,'[big]thicc[/big]'),(1,10,1,'2017-12-11 00:48:26',NULL,'[i]Try again[/i]'),(1,11,1,'2017-12-11 00:48:40',NULL,'[b] Bishal [/b]'),(1,12,2,'2017-12-11 00:54:20',NULL,'[QUOTE]\r\nAre traps gay?\r\n[/QUOTE]'),(1,13,2,'2017-12-11 00:56:07',NULL,'[CODE]\r\ndef shout(name):\r\n    if name == \"dragonrend\":\r\n        print \"Joor Zah Frul!\"\r\n[/CODE]\r\n'),(1,14,2,'2017-12-11 00:56:54',NULL,'[list=A]\r\n[*]First item\r\n[*]Second item\r\n[/list]'),(1,15,2,'2017-12-11 00:58:11',NULL,'Normal text.\r\nCheck if textarea handles CRLF correctly.'),(1,16,2,'2017-12-11 00:58:26',NULL,'Apparently not.\r\n'),(1,17,2,'2017-12-11 01:02:48',NULL,'Trying to insert HTML breaks.\r\nDoes it work now?'),(1,18,2,'2017-12-11 01:03:24',NULL,'[CODE]\r\ndef shout(name):\r\n    if name  == \'dragonrend\':\r\n        print(\"Joor Zah Frul!\")\r\n[/CODE]'),(1,19,2,'2017-12-11 01:04:52',NULL,'what \r\nis \r\nit'),(1,20,2,'2017-12-11 01:13:42',NULL,'What is the\r\n    problem');
+INSERT INTO `posts` VALUES (1,1,2,'2017-12-06 17:55:44',NULL,'Miraak? Is that you?'),(1,2,1,'2017-12-11 00:28:14',NULL,'Me: [b] FUS ROH DAH! [/b]\r\n[i]Testing BBcode parser [/i]'),(1,3,1,'2017-12-11 00:29:27',NULL,'<em>Trying again this time</em>.\r\n<strong> Fus Ro Dah! </strong>'),(1,4,1,'2017-12-11 00:34:04',NULL,'[url]htpp://www.fanfiction.net[/url]\r\n\r\n<span style=\"text-decoration:underline;\">What the?</span>.'),(1,5,1,'2017-12-11 00:34:22',NULL,'[link]http://www.example.com[/link]'),(1,6,1,'2017-12-11 00:35:23',NULL,'<em>Italics</em>'),(1,7,1,'2017-12-11 00:38:36',NULL,'[url href=\"http://www.fanfiction.net/alert/story.php\"] click me [/url]'),(1,8,1,'2017-12-11 00:39:46',NULL,'[url=\"http://www.fanfiction.net/alert/story.php\"] click me [/url]'),(1,9,1,'2017-12-11 00:40:08',NULL,'[big]thicc[/big]'),(1,10,1,'2017-12-11 00:48:26',NULL,'[i]Try again[/i]'),(1,11,1,'2017-12-11 00:48:40',NULL,'[b] Bishal [/b]'),(1,12,2,'2017-12-11 00:54:20',NULL,'[QUOTE]\r\nAre traps gay?\r\n[/QUOTE]'),(1,13,2,'2017-12-11 00:56:07',NULL,'[CODE]\r\ndef shout(name):\r\n    if name == \"dragonrend\":\r\n        print \"Joor Zah Frul!\"\r\n[/CODE]\r\n'),(1,14,2,'2017-12-11 00:56:54',NULL,'[list=A]\r\n[*]First item\r\n[*]Second item\r\n[/list]'),(1,15,2,'2017-12-11 00:58:11',NULL,'Normal text.\r\nCheck if textarea handles CRLF correctly.'),(1,16,2,'2017-12-11 00:58:26',NULL,'Apparently not.\r\n'),(1,17,2,'2017-12-11 01:02:48',NULL,'Trying to insert HTML breaks.\r\nDoes it work now?'),(1,18,2,'2017-12-11 01:03:24',NULL,'[CODE]\r\ndef shout(name):\r\n    if name  == \'dragonrend\':\r\n        print(\"Joor Zah Frul!\")\r\n[/CODE]'),(1,19,2,'2017-12-11 01:04:52',NULL,'what \r\nis \r\nit'),(1,20,2,'2017-12-11 01:13:42',NULL,'What is the\r\n    problem'),(1,21,2,'2017-12-11 01:48:19',NULL,'[url=\"http://www.fanfiction.net\"][/url]');
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -108,4 +108,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-11  1:18:34
+-- Dump completed on 2017-12-11  8:44:15
